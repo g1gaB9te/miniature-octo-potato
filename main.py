7653c69757e1533f041c55166469a53f9af40316
g1gaB9te <mordovecgleb12@gmail.com>
2024-12-20 22:40:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+response = requests.get("https://wise.com/gb/currency-converter/usd-to-kzt-rate?amount=1")
+if response.status_code == 200:
+ soup = BeautifulSoup(response.text,features="html.parser")
+ soup_list = soup.find_all("span", {"class": "text-success"})
+ item = soup_list[0].get_text(strip=True)
+ exchange_rate = float(item)
+ a = int(input())
+ b = a * exchange_rate
+ print(b)
+
+
+
